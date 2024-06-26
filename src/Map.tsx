90bba09e595720c658abde2067f9caaafce053d5
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-06-25 22:02:09 -0700

    Real basic map

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,21 +1,28 @@
 import { Drawer as Vaul } from 'vaul';
 import React from 'react';
+import maplibregl, { Map } from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 export default function UnderfootMap() {
   const [snap, setSnap] = React.useState<number | string | null | undefined>("100px");
+  const mapContainer = React.useRef<HTMLDivElement>(null);
+  const map = React.useRef<Map>();
+
+  React.useEffect( ( ) => {
+    if ( map.current ) return;
+    if ( !mapContainer.current ) return;
+
+    map.current = new maplibregl.Map( {
+      container: mapContainer.current,
+      center: [-122, 38],
+      zoom: 2,
+      style: 'https://demotiles.maplibre.org/style.json'
+    } );
+  }, [map, mapContainer] );
+
   return (
-    <div
-      style={{
-        backgroundColor: "lightyellow",
-        width: "100%",
-        height: "100%",
-        position: "fixed",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <p>Map goes here</p>
+    <div className="map-wrapper">
+      <div className="map" ref={mapContainer} />
       <Vaul.Root
         open
         snapPoints={["100px", 1]}
