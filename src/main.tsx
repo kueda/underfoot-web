bb7c3c791b8a20c0e0428535dd6f2ee0365908b4
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-08-20 22:01:08 -0700

    Attempt to prompt user to update when refresh needed

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// @ts-expect-error Virtual modules seem to confuse TS
+import { registerSW } from 'virtual:pwa-register'
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+if ("serviceWorker" in navigator) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const updateSW = registerSW({
+    onNeedRefresh() {
+      if ( confirm( "A new version is availble. Do you want to update?" ) ) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        updateSW( true );
+      }
+    },
+    onOfflineReady() {
+      alert( "Ready to work offline" );
+    },
+  })
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
